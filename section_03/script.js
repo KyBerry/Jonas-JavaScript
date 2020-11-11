31b01120225ae5961e07ebd39c0297c47a66ea0e
Kyle Berry <kyleberry@Kyles-MacBook-Pro.local>
2020-11-11 14:41:44 -0700

    Added array section in section_03 Added and completed challenge_2

--- section_03/script.js
+++ section_03/script.js
@@ -79,6 +79,64 @@ console.log(fruitJuice(2,3));
 
 
 
+// Arrays -> index list that starts at 0.
+// First way to write array
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+// Second way to write array
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+// Returns length of array | how many items are present
+console.log(friends.length); // output> 3
+
+// Get last element in the array 
+console.log(friends[friends.length - 1]); // output> 'Peter'
+
+// Calculate age with array | Exercise
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const calcAgeArr = function(birthYear) {
+
+}
+
+
+
+// Array method basics
+const friends2 = ['Michael', 'Steven', 'Peter'];
+
+// Add array elements | array methods
+// .push() method adds new element to end of the array and returns a new length.
+const newLength = friends2.push('Jay');
+console.log(friends2);
+console.log(newLength);
+// .unshift() method adds new element to front of array returns a new length.
+friends2.unshift('John');
+console.log(friends2);
+
+// Remove array elements | array methods
+// .pop() removes the last element of the array | returns removed item
+const el = friends2.pop();
+console.log(el);
+console.log(friends2);
+// .shift() removes first element in the array | returns removed item
+const shiftEl = friends2.shift();
+console.log(shiftEl);
+console.log(friends2);
+
+// Locate and return index of element if present in the array
+// .indexOf() returns position of element in the array | returns -1 if not present
+console.log(friends2.indexOf('Steven'));
+// ES6 method .includes() will return true or false if element is in the array
+console.log(friends2.includes('Steven'));
+// We can use includes to write conditionals
+if (friends2.includes('Peter')) {
+	console.log('You have a friend called Peter!');
+}
+
+
 
 
 
